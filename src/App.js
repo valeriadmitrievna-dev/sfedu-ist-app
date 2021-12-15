52a1bf1c5d353e2d99404785bb1e8985b317a738
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-12-15 23:48:12 +0300

    Add notification

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { useRoutes } from "./routes";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { notification } from "antd";
 
 const App = () => {
   const { isAuth } = useSelector(state => state.root);
   const routes = useRoutes(true);
+
+  useEffect(() => {
+    notification.info({
+      message: "Test version",
+      description:
+        "This is test version of project without working with database. Here only used hardcoded data. Wait for updates!",
+    });
+  }, []);
+
   return routes;
 };
 
