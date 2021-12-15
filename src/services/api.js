37b3fd26894bf637c1000abe218fcc4e67a681df
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-12-15 23:50:07 +0300

    Update base url of api

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    process.env.REACT_APP_NODE === "production"
-      ? process.env.REACT_APP_API_ORIGIN
-      : process.env.REACT_APP_API_DEV,
+  baseURL: process.env.REACT_APP_API,
   responseType: "json",
   withCredentials: true,
 });
@@ -12,10 +9,7 @@ const instance = axios.create({
 instance.interceptors.request.use(function (config) {
   const token = localStorage.getItem("access token");
   config.headers.Authorization = `Bearer ${token}`;
-  config.headers["Access-Control-Allow-Origin"] =
-    process.env.REACT_APP_NODE === "production"
-      ? process.env.REACT_APP_API_ORIGIN
-      : process.env.REACT_APP_API_DEV;
+  config.headers["Access-Control-Allow-Origin"] = process.env.REACT_APP_API;
   return config;
 });
 
